ffcdd909e65fde0bfe5a029723d07ed1845ee157
alpererdin <alpererdin636@hotmail.com>
2025-10-02 01:10:34 +0200

    dist

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,16 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const package = require("./package.json");
-// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
 
 module.exports = {
   entry: "./src/index.js",
-  resolve: {
-    extensions: [".js"],
-    alias: {
-      "@assets": path.resolve(__dirname, "./public/assets"),
-    },
-    fallback: { path: false },
-  },
-  experiments: {
-    topLevelAwait: true,
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name].[chunkhash].js",
+    chunkFilename: "[name].[chunkhash].js",
+    clean: true,
   },
   mode: "development",
-  //   devtool: "inline-source-map",
-  module: {
-    rules: [
-
-    ],
-  },
   devServer: {
     static: {
       directory: path.join(__dirname, "./public"),
@@ -32,29 +20,22 @@ module.exports = {
     open: true,
     port: 8008,
   },
-
-
-  optimization: {
-    minimize: true,
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendor",
-          enforce: true,
-          chunks: "initial",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
         },
       },
-    },
+    ],
   },
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "[name].[chunkhash].js",
-    chunkFilename: "[name].[chunkhash].js",
-    clean: true,
-  },
-  devServer: {
-    static: path.resolve(__dirname, "./dist"),
+  resolve: {
+    extensions: [".js"],
   },
   plugins: [
     new CopyWebpackPlugin({
@@ -70,8 +51,6 @@ module.exports = {
       filename: "index.html",
       title: package.description,
       inject: "body",
-      hot: true,
     }),
-    //  new BundleAnalyzerPlugin()
   ],
 };
